847539d85727744c58f0eaa9d926fc8348bc2b2a
Albert Kerr <akerr@L07690.local>
2021-09-08 22:16:48 +1000

    Add parking service test

--- test/test_parkingservice.py
+++ test/test_parkingservice.py
@@ -1,1 +1,19 @@
+from parkingservice.car import Car
+from parkingservice.employee import Employee
+from parkingservice.parking_service import ParkingService
+
+
 class TestParkingService:
+    def test_parking_service(self):
+        employees = [Employee("A", 0.11), Employee("B", 0.15)]
+        parking_service = ParkingService(len(employees))
+        cars = [
+            Car("A", "large", 0.07, 57),
+            Car("B", "large", 0.59, 66),
+            Car("C", "large", 0.49, 54),
+        ]
+        for employee in employees:
+            parking_service.add_employee(employee)
+
+        for car in cars:
+            parking_service.park_car(car)
